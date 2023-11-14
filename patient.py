b00361f8600cc7018dd9d95ca58c8b6d607b13b3
Joelle D <joelledamico24@gmail.com>
2023-11-13 23:20:38 -0500

    Updated to save user data

--- patient.py
+++ patient.py
@@ -3,6 +3,7 @@ import os.path
 import csv
 import pandas as pd
 import global_vars
+pd.options.mode.chained_assignment = None  # default='warn'
 
 
 ## PATIENT OBJECT
@@ -22,13 +23,13 @@ def createPD():
     if(checkFile == False):
         with open('patientData.csv', 'w',newline='') as csvfile:
             patientInfo = csv.writer(csvfile, delimiter=' ')
-            patientInfo.writerow(['Username', 'Password'])
+            patientInfo.writerow(['Username', 'Password', 'Parameters'])
     
 ## ADDS NEW VALID PATIENT TO CSV
 def addPatient(patientObj):
     with open('patientData.csv', 'a', newline='') as csvfile:
         patientInfo = csv.writer(csvfile, delimiter=' ')
-        patientInfo.writerow([str(patientObj.username), str(patientObj.password)])
+        patientInfo.writerow([str(patientObj.username), str(patientObj.password), "null"])
     print(availUsers())
 
 ## VALIDATES UNIQUE USERNAME       
@@ -70,9 +71,35 @@ def saveParams(param_vals, user):
     with open('patientData.csv', 'r') as csvfile:
         userName = pd.read_csv(csvfile, delimiter=' ')
         col_1 = userName['Username']
+        col_2 = userName['Password']
+        col_3 = userName['Parameters']
+        parseRow = 0
         for patients in range(col_1.size):
             if(user != str(col_1.values[patients])):
                 continue
             if(user == str(col_1.values[patients])):
                 print("Patient " + str(user) + " found on line " + str(patients+2) + " in database. This is user number " + str(patients+1))
                 print("Note that Username and Password are on the 1st row of the database. First user begins on 2nd line.")
+                parseRow = patients+2
+                print(parseRow)
+                #userName['Parameters'][patients] = ""
+                #print(userName)
+                ##userName.fillna("null")
+                print(userName)
+                print("Part 1")
+                #userName['Parameters'][patients] = str(param_vals)
+                #userName['Parameters'] = [param_vals]
+                userName['Parameters'][patients] = pd.to_numeric(['Parameters'], errors='coerce')
+                print(userName)
+                print("Part 2")
+                userName = userName.fillna("null")
+                print(userName)
+                print("Part 3")
+                userName['Parameters'][patients] = str(param_vals)
+                print(userName)
+                print("Part 4")
+                userName.to_csv('patientData.csv', index=False, sep=" ")
+                 
+                
+                
+                
