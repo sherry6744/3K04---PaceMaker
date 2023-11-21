56636a709049b6ae1d5437eee94765248bb08f34
Joelle D <joelledamico24@gmail.com>
2023-11-20 22:42:10 -0500

    Update patient.py
    updating to match main_3k04

--- patient.py
+++ patient.py
@@ -66,7 +66,8 @@ def availUsers():
         userName = pd.read_csv(csvfile, delimiter=' ') 
         col = userName['Username']
     return int(10-col.size)
-    
+
+## SAVE DATA TO CSV
 def saveParams(param_vals, user):
     with open('patientData.csv', 'r') as csvfile:
         userName = pd.read_csv(csvfile, delimiter=' ')
@@ -82,23 +83,35 @@ def saveParams(param_vals, user):
                 print("Note that Username and Password are on the 1st row of the database. First user begins on 2nd line.")
                 parseRow = patients+2
                 print(parseRow)
-                #userName['Parameters'][patients] = ""
-                #print(userName)
-                ##userName.fillna("null")
-                print(userName)
-                print("Part 1")
-                #userName['Parameters'][patients] = str(param_vals)
-                #userName['Parameters'] = [param_vals]
                 userName['Parameters'][patients] = pd.to_numeric(['Parameters'], errors='coerce')
-                print(userName)
-                print("Part 2")
                 userName = userName.fillna("null")
-                print(userName)
-                print("Part 3")
-                userName['Parameters'][patients] = str(param_vals)
-                print(userName)
-                print("Part 4")
+                userName['Parameters'][patients] = param_vals
                 userName.to_csv('patientData.csv', index=False, sep=" ")
+                
+## LOAD USER SAVED PARAMETERS
+def set_val(user):
+    with open('patientData.csv', 'r') as csvfile:
+        userName = pd.read_csv(csvfile, delimiter=' ')
+        col_1 = userName['Username']
+        col_2 = userName['Password']
+        col_3 = userName['Parameters']
+        user_params = "None"
+        print(global_vars.curr_user)
+        print("test"+ col_3.values[0])
+        parseRow = 0
+        #print(userName['Parameters'][patients])
+        for patients in range(col_1.size):
+            if(user != str(col_1.values[patients])):
+                continue
+            if(user == str(col_1.values[patients])):
+                if str(col_3.values[patients]) == "nan":
+                    print("not loading values.")
+                elif str(col_3.values[patients]) != "null":
+                    print("Loading Values...")
+                    user_params = col_3.values[patients]
+                    print(type(user_params))
+                    
+        return user_params
                  
                 
                 
